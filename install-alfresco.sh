6051948325dc024185d4db768424ee80b9743fc9
Richard Esplin <richard-oss@esplins.org>
2017-11-23 13:28:01 -0700

    Adding note about pg_hba.conf

--- install-alfresco.sh
+++ install-alfresco.sh
@@ -69,6 +69,7 @@ function setup_pghba {
     echo "Adding to pg_hba.conf"
     # I can't get this to only affect the first occurance, so watch out for
     # multiple markers in the file.
+    # Order matters, so it has to be above the default config
     sudo sed -i "/# TYPE  DATABASE        USER            ADDRESS                 METHOD/a \
 local  ${ALF_DB}    ${ALF_USER}                   md5 \\
 host   ${ALF_DB}    ${ALF_USER}     127.0.0.1/32  md5 "\
